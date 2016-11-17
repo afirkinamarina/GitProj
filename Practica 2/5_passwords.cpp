506a2784f9cd9a5edac7b6d1ed13d799a93bc090
Marina Afirkina <afirkinamarina@yandex.ru>
2016-11-18 00:50:32 +0300

    practika2_5

--- Practica 2/5_passwords.cpp
+++ Practica 2/5_passwords.cpp
@@ -28,45 +28,46 @@ int main()
 			newstring[k] = exchange;
 		}
 	}
+	//record each password in an array
 	char pass1[20], pass2[20], pass3[20], pass4[20], pass5[20], pass6[20], pass7[20], pass8[20], pass9[20], pass10[20];
-	strncpy(pass1, newstring, 10);
-	pass1[10] = 0;
+	strncpy(pass1, newstring, len);
+	pass1[len] = 0;
 	printf("%s", pass1);
 
-	strncpy(pass2, newstring+10, 10);
-	pass2[10] = 0;
+	strncpy(pass2, newstring+10, len);
+	pass2[len] = 0;
 	printf("\n%s", pass2);
 
-	strncpy(pass3, newstring + 20, 10);
-	pass3[10] = 0;
+	strncpy(pass3, newstring + 20, len);
+	pass3[len] = 0;
 	printf("\n%s", pass3);
 
-	strncpy(pass4, newstring + 30, 10);
-	pass4[10] = 0;
+	strncpy(pass4, newstring + 30, len);
+	pass4[len] = 0;
 	printf("\n%s", pass4);
 
-	strncpy(pass5, newstring + 40, 10);
-	pass5[10] = 0;
+	strncpy(pass5, newstring + 40, len);
+	pass5[len] = 0;
 	printf("\n%s", pass5);
 
-	strncpy(pass6, newstring + 50, 10);
-	pass6[10] = 0;
+	strncpy(pass6, newstring + 50, len);
+	pass6[len] = 0;
 	printf("\n%s", pass6);
 
-	strncpy(pass7, newstring + 60, 10);
-	pass7[10] = 0;
+	strncpy(pass7, newstring + 60, len);
+	pass7[len] = 0;
 	printf("\n%s", pass7);
 
-	strncpy(pass8, newstring + 70, 10);
-	pass8[10] = 0;
+	strncpy(pass8, newstring + 70, len);
+	pass8[len] = 0;
 	printf("\n%s", pass8);
 
-	strncpy(pass9, newstring + 80, 10);
-	pass9[10] = 0;
+	strncpy(pass9, newstring + 80, len);
+	pass9[len] = 0;
 	printf("\n%s", pass9);
 
-	strncpy(pass10, newstring + 90, 10);
-	pass10[10] = 0;
+	strncpy(pass10, newstring + 90, len);
+	pass10[len] = 0;
 	printf("\n%s", pass10);
 		
 	getchar();
