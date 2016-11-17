b4c30b9fa94a63005f26f94a99b6c48466ee2c38
Marina Afirkina <afirkinamarina@yandex.ru>
2016-11-18 01:40:45 +0300

    Pr2_12345

--- Practica 2/4_sort.cpp
+++ Practica 2/4_sort.cpp
@@ -6,6 +6,7 @@
 int main()
 {
 	//line generation
+
 	int choose, len = 8;
 	srand(time(NULL));
 	char exchange, newstring[8];
@@ -27,14 +28,15 @@ int main()
 	char newstr[11];
 	strncpy(newstr, newstring, 11);
 	newstr[len] = 0; //newstr - the resulting string
-	printf("%s\n", newstr);
+	printf("String:\n%s\n", newstr);
+	printf("Grouped string:\n");
 	char exchange2;
 	//permutation of characters
 	for (int i = 0; i < len; i++)
 		if (newstr[i] < '0' || newstr[i] > '9')
 		{
 			exchange2 = newstr[i];
-			printf("%c", o);
+			printf("%c", exchange2);
 		}
 	for (int j = 0; j <len; j++)
 		if (newstr[j] >= '0' && newstr[j] <= '9')
