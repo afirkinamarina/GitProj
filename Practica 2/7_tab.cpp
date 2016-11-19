f1286e4ee6650fb3c7ac2bba0e70081ccae6c075
Marina Afirkina <afirkinamarina@yandex.ru>
2016-11-19 14:11:02 +0300

    practica 2_7

---
+++ Practica 2/7_tab.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+	int const n = 256;
+	char t[300] = "", str[300] = "";
+	int counter = 0, counter1 = 0;
+	printf("Input string: ");
+	scanf("%s", &str);
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		counter = 1;
+		for (int k = 0; str[k] != '\0'; k++)
+		{
+			if (str[i] == str[k])
+				counter++;
+		}
+		counter1 = 0;
+		for (int k = (i+1); str[k] != '\0'; k++)
+		{
+			if (str[i] == str[k])
+				counter1++;
+		}
+		if ((counter1 < 1) && (str[i] != '\n'))
+			printf("|%c |%i |\n", str[i], counter - 1);
+	}
+	getchar();
+	getchar();
+	return 0;
+}
