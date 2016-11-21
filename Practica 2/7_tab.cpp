a47a7337ca66905faf8996ee2b6130986ffa59d4
Marina Afirkina <afirkinamarina@yandex.ru>
2016-11-22 02:07:16 +0300

    2 update 3

--- Practica 2/7_tab.cpp
+++ Practica 2/7_tab.cpp
@@ -2,21 +2,17 @@
 int main()
 {
 	int const n = 300;
-	char t[300] = "", str[300] = "";
+	char str[n] = "";
 	int counter = 0, counter1 = 0;
 	printf("Input string: ");
 	fgets(str, n, stdin);
-
 	for (int i = 0; str[i] != '\0'; i++)
 	{
 		counter = 1;
 		for (int k = 0; str[k] != '\0'; k++)
 		{
 			if (str[i] == str[k])
-
-			{
 				counter++;
-			}
 		}
 		counter1 = 0;
 		for (int k = (i+1); str[k] != '\0'; k++)
