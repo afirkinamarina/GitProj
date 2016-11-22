5b4fd216b1ba9e3e821c8a75de1b22e42fd67c7d
Marina Afirkina <afirkinamarina@yandex.ru>
2016-11-22 21:23:01 +0300

    pr 3 _1234

--- Practica 3/1_wrds.cpp
+++ Practica 3/1_wrds.cpp
@@ -7,15 +7,17 @@ int main()
 	int counter = 0;
 	printf("Enter the string: ");
 	fgets(str, 100, stdin);
-	for (int i = 0; i < strlen(str); i++)
+	char *s = str;
+	while (*s)
 	{
-		if (isspace(str[i]) && !isspace(str[i + 1]))
+		if (isspace(*s) && !isspace(*(s+1)))
 			counter++;
-		else if (isspace(str[i]) && !isspace(str[i + 1]))
-			i++;
+		else if (isspace(*s) && !isspace(*(s+1)))
+			*s++;
+		*s++;
 	}
 	if (isspace(str[0]))
-		printf("The number of words: %i", counter-1);
+		printf("The number of words: %i", counter - 1);
 	else
 		printf("The number of words: %i", counter);
 	getchar();
---
+++ Practica 3/2_all words.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h> 
+int main()
+{
+	char str[300] = "";
+	int len = 0, counter = 0;
+	printf("Enter the string: ");
+	fgets(str, 300, stdin);
+	char *s = str;
+	printf("Words:\n\n");
+	while (*s)
+	{
+		if (!isspace(*s))
+		{
+			printf("%c", *s);
+			len++;
+			if (isspace(*(s + 1)))
+			{
+				printf(" %i simbols.\n", len);
+				len = 0;
+			}
+		}
+		if (isspace(*s) && !isspace(*(s + 1)))
+			counter++;
+		else if (isspace(*s) && !isspace(*(s + 1)))
+			*s++;
+		*s++;
+	}
+	if (isspace(str[0]))
+		printf("\nThe number of words: %i", counter - 1);
+	else
+		printf("\nThe number of words: %i", counter - 1);
+	getchar();
+	return 0;
+}
+
